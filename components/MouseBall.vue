881b3e0b9647a378caf970cf1e28c54aabfa34ad
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-02-29 15:12:49 -0500

    feat: update mouse ball

--- components/MouseBall.vue
+++ components/MouseBall.vue
@@ -5,14 +5,13 @@ const { x, y } = useMouse()
 </script>
 <template>
   <div
-    class="absolute rounded-full bg-gray-400 dark:bg-shark-500 -translate-x-1/2 -translate-y-1/2 pointer-events-none mouse-ball"
+    class=" mouse-ball"
     :style="{ left: `${x}px`, top: `${y}px` }"
   />
 </template>
 <style lang="scss" scoped>
 .mouse-ball{
+  @apply size-[200px] absolute -translate-x-1/2 -translate-y-1/2 bg-gray-400 rounded-full pointer-events-none dark:bg-shark-400;
   filter: blur(148px);
-  width: 200px;
-  height: 200px;
 }
 </style>
