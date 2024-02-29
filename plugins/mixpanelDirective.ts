f8f0d9617fc665b4dce59264f74ed4241fc63a13
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-02-29 15:09:27 -0500

    feat: add mixpanel plugin

---
+++ plugins/mixpanelDirective.ts
@@ -0,0 +1,23 @@
+import mixpanel from 'mixpanel-browser'
+import type { IMixpanelDirective } from '@/interfaces/IMixpanel'
+
+interface MixpanelDirectiveBinding {
+    value: IMixpanelDirective
+}
+
+const trackMixpanel = (data: IMixpanelDirective) => {
+    const { id, name, payload } = data
+    mixpanel.identify(`${id}`)
+    mixpanel.track(name, payload)
+    console.log(`===== Event tracked: ${name} =====`)
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.directive('mixpanel', {
+        mounted(el: HTMLElement, binding: MixpanelDirectiveBinding){
+            el.addEventListener('click', () => {
+                trackMixpanel(binding.value)
+            })
+        }
+    })
+})
