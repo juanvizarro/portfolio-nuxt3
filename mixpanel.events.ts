6acf448b0007c8ed2483e8b4052ed012b94edf2b
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-05 16:59:13 -0500

    feat: create first mixpanel events

---
+++ mixpanel.events.ts
@@ -0,0 +1,15 @@
+import type { IMixpanelDirective } from '@/interfaces/IMixpanel'
+
+export const CONTACT_LINKEDIN_EVENT = <IMixpanelDirective>{
+    name: 'contact-linkedin',
+    payload: {}
+}
+
+export const CONTACT_GITHUB_EVENT = <IMixpanelDirective>{
+    name: 'contact-github',
+    payload: {}
+}
+export const CONTACT_EMAIL_EVENT = <IMixpanelDirective>{
+    name: 'contact-email',
+    payload: {}
+}
